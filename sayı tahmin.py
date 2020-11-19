02a0b1db7ee30098aa09ceb785c419ff5118b5c0
hafsaeskiyurt <74589627+hafsaeskiyurt@users.noreply.github.com>
2020-11-19 18:45:30 +0300

    Update sayı tahmin.py

--- sayı tahmin.py
+++ sayı tahmin.py
@@ -1,6 +1,6 @@
 
 
-a=int(input("baþlamak için 1 tuþuna çýkýþ yapmak için 2 tuþuna basýnýz : "))
+a=int(input("baslamak için 1 tusuna çikiþ yapmak için 2 tusuna basiniz : "))
 
     
 def sayitahmin():
@@ -8,33 +8,33 @@ def sayitahmin():
     from random import randint
     sayi=randint(1,100)
 
-    sayac=0     #kullanicinin tahmin sayisini bulabilmek için bir sayaç oluþturduk ve baþlangýç deðeri olarak 0 verdik.
-    tahmin=int(input("1 ile 100 arasýnda bir sayý girin : "))
+    sayac=0     #kullanicinin tahmin sayisini bulabilmek için bir sayaç olusturduk ve baslangiç degeri olarak 0 verdik.
+    tahmin=int(input("1 ile 100 arasinda bir sayi girin : "))
     while True:
         
         if tahmin<sayi:
-            print("daha yüksek bir sayý gir")
+            print("daha yüksek bir sayi gir")
             sayac+=1          #sayaci 1 arttirdik çünkü kullanici tahminde bulundu.
-            tahmin=int(input("1 ile 100 arasýnda bir sayý girin : "))
+            tahmin=int(input("1 ile 100 arasinda bir sayi girin : "))
 
 
         elif tahmin>sayi:
-            print("daha küçük bir sayý gir")
+            print("daha küçük bir sayi gir")
             sayac+=1
-            tahmin=int(input("1 ile 100 arasýnda bir sayý girin : "))
+            tahmin=int(input("1 ile 100 arasinda bir sayi girin : "))
 
 
         elif tahmin==sayi:
             sayac+=1
             print("tebrikler doðru bildiniz")
-            print("tahmin sayýnýz : {}".format(sayac))
-            break        #ekrana sürekli ayný þeyi yazdýrmamasý için "break" yazdýk.
+            print("tahmin sayiniz : {}".format(sayac))
+            break        #ekrana sürekli ayni seyi yazmamasi için "break" kullandik.
 
 while True:
     if a==1:
         sayitahmin()
-        a=int(input("baþlamak için 1 tuþuna çýkýþ yapmak için 2 tuþuna basýnýz : "))
+        a=int(input("baslamak için 1 tusuna çikis yapmak için 2 tusuna basiniz : "))
         
     elif a==2:
-        print("çýkýþ yapýlýyor")
+        print("çikis yaptiniz")
         break
