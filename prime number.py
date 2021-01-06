6be02404fac4ae1a86340605a7b460b309fc4a95
hafsaeskiyurt <74589627+hafsaeskiyurt@users.noreply.github.com>
2021-01-06 11:09:44 +0300

    Add files via upload
    burada bunu yaptım

--- prime number.py
+++ prime number.py
@@ -3,10 +3,10 @@ counter=0
 
 for i in range(2,a+1):
     
-    if a%i==0: #if the number is prime number, it can only divided itself and one.
+    if a % i ==0: #if the number is prime number, it can only divided itself and one.
         counter+=1 #since the number will only divided to itself, the programme will add plus 1 to the counter. 
         
-if counter==1:
+if counter == 1:
     print("This number is prime number.")
 else:
     print("This number isn't prime number.")
